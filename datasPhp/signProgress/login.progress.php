8e4db58ad94f7dc29ad9fbae6e810bf1ef6cd4b2
loonthink <13007650312@163.com>
2018-11-30 09:58:58 +0800

    配合设置虚拟主机

--- datasPhp/signProgress/login.progress.php
+++ datasPhp/signProgress/login.progress.php
@@ -1,6 +1,7 @@
 <?php
 
 	header('Access-Control-Allow-Origin: http://localhost:8080');
+	header('Access-Control-Allow-Origin: http://sign.com:99');
 	header("Access-Control-Allow-Credentials: true");
 
 	session_start();
